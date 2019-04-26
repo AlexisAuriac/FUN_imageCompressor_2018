66ae81b7ef2f4089276323e26bcdef0c40c93b3c
alexis <alexis.auriac@epitech.eu>
2019-04-26 21:06:16 +0200

    Moved kmeans main algo from Main.hs to src/Kmeans.hs

--- app/Main.hs
+++ app/Main.hs
@@ -6,38 +6,8 @@ import Control.Exception
 
 import Utility
 import Params
-import Pixel
-import Color
-import Centroid
 import Cluster
-
-clusterColorPart :: (Color -> Int) -> Cluster -> [Int]
-clusterColorPart access cluster = map getColorByte pixels
-    where
-        pixels = clusterPixels cluster
-        getColorByte pixel = access $ pixelColor pixel
-
-averageCentroid :: Cluster -> Centroid
-averageCentroid cluster = Centroid averageR averageG averageB
-    where
-        averageR = average $ clusterColorPart colorR cluster
-        averageG = average $ clusterColorPart colorG cluster
-        averageB = average $ clusterColorPart colorB cluster
-
-updateCentroids :: [Cluster] -> [Centroid]
-updateCentroids clusters = map averageCentroid clusters
-
-updateClusters :: [Cluster] -> [Pixel] -> [Cluster]
-updateClusters clusters pixels = getClusters centroids pixels
-    where
-        centroids = updateCentroids clusters
-
-kmeans :: [Cluster] -> [Pixel] -> Float -> [Cluster]
-kmeans clusters pixels convLim
-    | newClusters == clusters = clusters
-    | otherwise = kmeans newClusters pixels convLim
-    where
-        newClusters = updateClusters clusters pixels
+import Kmeans
 
 imgCompressor :: IO ()
 imgCompressor = do
